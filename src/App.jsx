507abda4627daf7a3b61cc749bb415a228191cc8
ivk101 <irinaislamova100@gmail.com>
2021-04-04 19:50:03 +0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import styles from './app.css'
+
+
+export function App () {
+	return (
+	    <div>
+	        <h2 className={styles.example}>Hello React</h2>	        
+	    </div>
+	);
+}
